26572a7a5ccd8657842de42f8e0776651f099458
Victor Gwee <47884004+vicgwee@users.noreply.github.com>
2021-04-13 17:02:49 +0800

    adaptive control working

--- Project 2/Sec4/param.m
+++ Project 2/Sec4/param.m
@@ -1,5 +1,5 @@
 % Compute the control parameters 
-clear all
+clear
 
 %reference model params
 ratio_m = 1; 
@@ -13,3 +13,14 @@ P       = lyap(A_m, Q);
 %adaptive law params
 Gamma = diag([1,1]);
 gamma = 10; 
+
+%calculate perfect gains
+a = [6.2];
+b = [0.25 1];
+[w_p, ratio_p] = damp(tf(a,b));
+A_p = [0, 1;
+      -w_p^2, -2*ratio_p*w_p];
+g = w_p^2;
+
+theta_x_p = [0, 1] * (A_m - A_p)/g;
+theta_r_p = w_m^2/g;
